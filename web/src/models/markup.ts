e35dafb082f9e2dd43404ad7a2483507c15ecee5
Николай Славнов <n.slavnov@kod-it.ru>
2022-10-27 19:04:04 +0300

    feat: added types for markup model, updated entity form and validation

--- web/src/models/markup.ts
+++ web/src/models/markup.ts
@@ -10,12 +10,42 @@ export interface NewMarkupDto {
   record?: number;
 }
 
+export type EntityType = 'Allophone' | 'Word' | 'Sentence' | undefined;
+
+export interface AllophoneProperties {
+  stressId: number;
+}
+
+export interface WordProperties {
+  languageId: number;
+  dialect?: string;
+}
+
+export interface SentenceProperties {
+}
+
 export interface EntityDto {
   id?: number;
   markupId: number;
   value: string;
   beginTime: number;
   endTime: number;
+  type: EntityType;
+  properties: AllophoneProperties | WordProperties | SentenceProperties;
+}
+export type CreateEntityDto = Omit<EntityDto, 'id'>
+
+export interface AllophoneDto extends EntityDto {
+  type: 'Allophone';
+  properties: AllophoneProperties;
 }
 
-export type CreateEntityDto = Omit<EntityDto, 'id'>
+export interface WordDto extends EntityDto {
+  type: 'Word';
+  properties: WordProperties;
+}
+
+export interface SentenceDto extends EntityDto {
+  type: 'Sentence';
+  properties: SentenceProperties;
+}
