9df2a3b00732a595ac2e37ea9e8a7803d18aa046
Николай Славнов <n.slavnov@kod-it.ru>
2022-10-28 01:31:31 +0300

    fix: removed cringe

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <Box as="section" py="12">
-      NU UDA4I, 40
-    </Box>
+    <Flex p={10} justifyContent="center">
+      <p>Видишь ошибку на сайте? Пиши сюда 👉</p>
+      <Flex>
+        <img style={{padding: "0px 4px 0px 4px"}} src="https://telegram.org/img/website_icon.svg?4" />
+        @videot4pe @strooom
+      </Flex>
+    </Flex>
   );
 };
 
